eb0e7713a11740a4ed48765df83246ccf6dc0759
enrique050505 <enriquemartinalbodo@gmail.com>
2024-11-27 21:03:54 +0100

    Creacion de la pagina de inicio de sesion finalizada

---
+++ app/src/main/java/com/example/proyectout2/Inicio.java
@@ -0,0 +1,31 @@
+package com.example.proyectout2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Inicio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_inicio);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void salirAplicacion(View view) {
+        Intent i = new Intent(Inicio.this, MainActivity.class);
+        startActivity(i);
+    }
+}
--- app/src/main/java/com/example/proyectout2/MainActivity.java
+++ app/src/main/java/com/example/proyectout2/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.proyectout2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +15,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editTextUsuario;
+    EditText editTextEmail;
+    EditText editTextContraseña;
+    CheckBox checkBoxTerminosYCondiciones;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +31,34 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        editTextUsuario = findViewById(R.id.editText_Usuario);
+        editTextContraseña = findViewById(R.id.editText_Contraseña);
+        editTextEmail = findViewById(R.id.editText_CorreoElectronico);
+        checkBoxTerminosYCondiciones = findViewById(R.id.checkBox_TerminosYPrivacidad);
+    }
+
+    public void iniciarApp(View view){
+        String email = editTextEmail.getText().toString();
+        String contraseña = editTextContraseña.getText().toString();
+        String usuario = editTextUsuario.getText().toString();
+        boolean checkBoxActivado = checkBoxTerminosYCondiciones.isChecked();
+
+        if(email.isEmpty() && contraseña.isEmpty() && usuario.isEmpty() && !checkBoxActivado){
+            Toast.makeText(this, "Tienes que rellenar estos campos y aceptar las condiciones", Toast.LENGTH_SHORT).show();
+        }else {
+            if(email.isEmpty()){
+                Toast.makeText(this, "Introduzca su correo electrónico", Toast.LENGTH_SHORT).show();
+            }else if (contraseña.isEmpty()){
+                Toast.makeText(this, "Introduzca su contraseña", Toast.LENGTH_SHORT).show();
+            }else if (usuario.isEmpty()){
+                Toast.makeText(this, "Introduzca su nombre de usuario", Toast.LENGTH_SHORT).show();
+            }else if (!checkBoxActivado){
+                Toast.makeText(this, "Debes aceptar los términos y condiciones", Toast.LENGTH_SHORT).show();
+            }else{
+                Intent i = new Intent(MainActivity.this, Inicio.class);
+                startActivity(i);
+            }
+        }
     }
 }
