af7d681c716d2527b281ed204dd662dab837e41d
enrique050505 <enriquemartinalbodo@gmail.com>
2024-12-02 13:37:59 +0100

    He acabado el proyecto. Revision final hecha

--- app/src/main/java/com/example/proyectout2/Clasificacion.java
+++ app/src/main/java/com/example/proyectout2/Clasificacion.java
@@ -41,12 +41,10 @@ public class Clasificacion extends AppCompatActivity {
         editTextPuntosEquipo = findViewById(R.id.editText_PuntosEquipo);
         clasificacion = findViewById(R.id.ListView_Clasificacion);
 
+        clasificacionEquipos = new ArrayList<>();
+
         cargarClasificacion();
 
-        clasificacionEquipos = new ArrayList<>();
-        adaptadorClasificacion = new ArrayAdapter<>(this, R.layout.item_clasificacion, R.id.textView_ClasificacionEquipo, clasificacionEquipos);
-        clasificacion.setAdapter(adaptadorClasificacion);
-        clasificacion.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
 
         Toolbar toolbar = findViewById(R.id.toolbar_clasificacion);
         setSupportActionBar(toolbar);
@@ -58,19 +56,31 @@ public class Clasificacion extends AppCompatActivity {
             return insets;
         });
 
+        adaptadorClasificacion = new ArrayAdapter<>(this, R.layout.item_clasificacion, R.id.textView_ClasificacionEquipo, clasificacionEquipos);
+        clasificacion.setAdapter(adaptadorClasificacion);
+        clasificacion.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+
+        clasificacion.setOnItemClickListener((parent, view, position, id) ->{
+            String equipoSeleccionado = clasificacionEquipos.get(position);
+            String [] partesEquipo = equipoSeleccionado.split("º - | - ");
+            editTextPosicionEquipo.setText(partesEquipo[0]);
+            editTextNombreEquipo.setText(partesEquipo[1]);
+            editTextPuntosEquipo.setText(partesEquipo[2]);
+        });
+
     }
 
     public void cargarClasificacion(){
-        clasificacionEquipos.add("1º  FC Barcelona - 88");
-        clasificacionEquipos.add("2º  Real Madrid - 85");
-        clasificacionEquipos.add("3º  Liverpool - 80");
-        clasificacionEquipos.add("4º  Bayern Múnich - 76");
-        clasificacionEquipos.add("5º  Manchester City - 73");
-        clasificacionEquipos.add("6º  Atlético de Madrid - 68");
-        clasificacionEquipos.add("7º  AC Milan - 64");
-        clasificacionEquipos.add("8º  Inter de Milán - 58");
-        clasificacionEquipos.add("9º  Bayer Leverkusen - 55");
-        clasificacionEquipos.add("10º  Borussia Dortmund - 47");
+        clasificacionEquipos.add("1º - FC Barcelona - 88");
+        clasificacionEquipos.add("2º - Real Madrid - 85");
+        clasificacionEquipos.add("3º - Liverpool - 80");
+        clasificacionEquipos.add("4º -  Bayern Múnich - 76");
+        clasificacionEquipos.add("5º - Manchester City - 73");
+        clasificacionEquipos.add("6º - Atlético de Madrid - 68");
+        clasificacionEquipos.add("7º - AC Milan - 64");
+        clasificacionEquipos.add("8º - Inter de Milán - 58");
+        clasificacionEquipos.add("9º - Bayer Leverkusen - 55");
+        clasificacionEquipos.add("10º - Borussia Dortmund - 47");
 
     }
 
@@ -85,16 +95,16 @@ public class Clasificacion extends AppCompatActivity {
         String puntos = editTextPuntosEquipo.getText().toString();
 
         if(!nombreEquipo.isEmpty() && !posicion.isEmpty() && !puntos.isEmpty()){
-            String nuevoEquipo = String.format("%sº %s - %s", posicion, nombreEquipo, puntos);
+            String nuevoEquipo = (posicion + "º" + " - " + nombreEquipo + " - " + puntos);
 
-            if(clasificacionEquipos.contains(nuevoEquipo)){
+            if(!clasificacionEquipos.contains(nuevoEquipo)){
                 clasificacionEquipos.add(nuevoEquipo);
                 adaptadorClasificacion.notifyDataSetChanged();
                 editTextPuntosEquipo.setText("");
                 editTextNombreEquipo.setText("");
                 editTextPosicionEquipo.setText("");
                 Toast.makeText(this, "Clasificación actualizada", Toast.LENGTH_SHORT).show();
-            }else {
+            }else{
                 Toast.makeText(this, "El equipo está en la clasificación", Toast.LENGTH_SHORT).show();
             }
         }else{
@@ -103,18 +113,25 @@ public class Clasificacion extends AppCompatActivity {
     }
 
     public void eliminarEquipoClasificacion(View view){
-       int indice = clasificacion.getCheckedItemPosition();
+        String nombreEquipo = editTextNombreEquipo.getText().toString();
+        String posicion = editTextPosicionEquipo.getText().toString();
+        String puntos = editTextPuntosEquipo.getText().toString();
 
-       if(indice != ListView.INVALID_POSITION){
-           clasificacionEquipos.remove(indice);
-           adaptadorClasificacion.notifyDataSetChanged();
-           editTextPuntosEquipo.setText("");
-           editTextNombreEquipo.setText("");
-           editTextPosicionEquipo.setText("");
-           Toast.makeText(this, "Equipo eliminado de la clasificación", Toast.LENGTH_SHORT).show();
-       }else{
-           Toast.makeText(this, "Selecciona un equipo para eliminarlo de la clasificación", Toast.LENGTH_SHORT).show();
-       }
+        if(!nombreEquipo.isEmpty() && !posicion.isEmpty() && !puntos.isEmpty()){
+            int indice = clasificacion.getCheckedItemPosition();
+
+            if(indice != ListView.INVALID_POSITION){
+                editTextNombreEquipo.setText("");
+                editTextPosicionEquipo.setText("");
+                editTextPuntosEquipo.setText("");
+                clasificacionEquipos.remove(indice);
+                adaptadorClasificacion.notifyDataSetChanged();
+            }else{
+                Toast.makeText(this, "Selecciona un equipo para eliminarlo", Toast.LENGTH_SHORT).show();
+            }
+        }else{
+            Toast.makeText(this, "Selecciona un equipo para eliminarlo", Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void editarEquipoClasificacion(View view){
@@ -125,7 +142,7 @@ public class Clasificacion extends AppCompatActivity {
         if(!nombreEquipo.isEmpty() && !posicion.isEmpty() && !puntos.isEmpty()){
            int indice = clasificacion.getCheckedItemPosition();
            if(indice != ListView.INVALID_POSITION){
-               String equipoEditado = String.format("%sº %s - %s", posicion, nombreEquipo, puntos);
+               String equipoEditado =(posicion + "º" + " - " + nombreEquipo + " - " + puntos);
                clasificacionEquipos.set(indice, equipoEditado);
                adaptadorClasificacion.notifyDataSetChanged();
                editTextPuntosEquipo.setText("");
@@ -135,6 +152,8 @@ public class Clasificacion extends AppCompatActivity {
            }else{
                Toast.makeText(this, "Selecciona un equipo para editarlo de la clasificación", Toast.LENGTH_SHORT).show();
            }
+        }else{
+            Toast.makeText(this, "Selecciona un equipo para editarlo de la clasificación", Toast.LENGTH_SHORT).show();
         }
     }
 
